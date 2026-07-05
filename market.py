aaf353d059aa358ff876d720b24368c2ca1bfd84
infoversehub <bwdalhbwb74@gmail.com>
2026-07-05 05:42:48 +0300

    Update market.py

--- market.py
+++ market.py
@@ -1,1 +1,33 @@
-1
+import os
+import requests
+
+API_KEY = os.getenv("FINNHUB_API_KEY")
+
+
+def get_price(symbol):
+
+    url = "https://finnhub.io/api/v1/quote"
+
+    params = {
+        "symbol": symbol,
+        "token": API_KEY
+    }
+
+    try:
+        response = requests.get(url, params=params, timeout=10)
+
+        if response.status_code != 200:
+            return None
+
+        data = response.json()
+
+        return {
+            "price": data["c"],
+            "high": data["h"],
+            "low": data["l"],
+            "open": data["o"],
+            "previous_close": data["pc"]
+        }
+
+    except Exception:
+        return None
