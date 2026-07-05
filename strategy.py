1d5ec530652b650a737f4b017f49c7eba519b54c
infoversehub <bwdalhbwb74@gmail.com>
2026-07-05 05:45:48 +0300

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,1 +1,26 @@
+from market import get_price
 
+
+def generate_signal(symbol):
+
+    data = get_price(symbol)
+
+    if data is None:
+        return None
+
+    price = data["price"]
+    open_price = data["open"]
+
+    if price > open_price:
+        direction = "🟢 BUY"
+        confidence = 60
+    else:
+        direction = "🔴 SELL"
+        confidence = 60
+
+    return {
+        "direction": direction,
+        "confidence": confidence,
+        "entry": price,
+        "expiry": "1 Minute"
+    }
