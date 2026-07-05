42e937bec0c608d6649ea19cd9c824cee7353aca
infoversehub <bwdalhbwb74@gmail.com>
2026-07-05 05:42:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,29 +7,48 @@ from telegram.ext import (
     ContextTypes,
 )
 
+from market import get_price
+
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "🤖 Abod Signals Bot\n\n"
-        "البوت يعمل بنجاح ✅"
+        "🤖 Abod Signals\n\n"
+        "الأوامر:\n"
+        "/signal"
     )
 
 
 async def signal(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    data = get_price("OANDA:EUR_USD")
+
+    if data is None:
+        await update.message.reply_text("❌ فشل الاتصال بالسوق.")
+        return
+
     await update.message.reply_text(
-        "⏳ جاري تحليل السوق..."
+        f"""
+📊 EUR/USD
+
+السعر الحالي: {data['price']}
+أعلى سعر: {data['high']}
+أقل سعر: {data['low']}
+الافتتاح: {data['open']}
+إغلاق أمس: {data['previous_close']}
+"""
     )
 
 
 def main():
+
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("signal", signal))
 
-    print("Abod Signals Started")
+    print("Abod Signals Running...")
 
     app.run_polling()
 
