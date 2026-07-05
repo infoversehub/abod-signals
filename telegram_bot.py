a2d63729f836a15b11760ed095e1a1fa27508204
infoversehub <bwdalhbwb74@gmail.com>
2026-07-05 05:46:20 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,1 +1,21 @@
+import os
+import requests
 
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+
+def send_message(message):
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+
+    try:
+        requests.post(url, json=payload, timeout=10)
+        return True
+    except Exception:
+        return False
