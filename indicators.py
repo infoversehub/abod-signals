4e4c932fd4c46cf640a030de29547b67aeb5b2e1
infoversehub <bwdalhbwb74@gmail.com>
2026-07-05 05:46:59 +0300

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -1,1 +1,26 @@
-1
+import pandas as pd
+import ta
+
+
+def add_indicators(df):
+
+    df["EMA20"] = ta.trend.ema_indicator(df["close"], window=20)
+
+    df["EMA50"] = ta.trend.ema_indicator(df["close"], window=50)
+
+    df["EMA200"] = ta.trend.ema_indicator(df["close"], window=200)
+
+    df["RSI"] = ta.momentum.rsi(df["close"], window=14)
+
+    df["MACD"] = ta.trend.macd(df["close"])
+
+    df["MACD_SIGNAL"] = ta.trend.macd_signal(df["close"])
+
+    df["ATR"] = ta.volatility.average_true_range(
+        df["high"],
+        df["low"],
+        df["close"],
+        window=14
+    )
+
+    return df
